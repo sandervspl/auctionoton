7c19cf44896644e475d8292cf330ca82115de639
Sander Vispoel <sander.vispoel@labela.nl>
2020-05-18 18:43:41 +0200

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import { rollup } from 'rollup';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { chromeExtension, pushReloader } from 'rollup-plugin-chrome-extension';
+
+export default {
+  input: 'src/manifest.json',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+  },
+  plugins: [
+    // always put chromeExtension() before other plugins
+    chromeExtension(),
+    pushReloader(),
+    typescript(),
+    // the plugins below are optional
+    resolve(),
+    commonjs()
+  ],
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+type Test = 1;
+
+const num: Test = 1;
+
+console.log(num); 
