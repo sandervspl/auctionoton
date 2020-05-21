3e4ce5d04c775ac76799eb49da45fef476e7e02e
Sander Vispoel <sander.vispoel@labela.nl>
2020-05-21 20:38:57 +0200

    Set initial user data after install

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,18 @@
+import AsyncStorage from './asyncStorage';
+
 chrome.runtime.onInstalled.addListener((details) => {
   // Open page for user's server/faction information after installation
   if (details.reason === 'install') {
+    AsyncStorage.set({
+      user: {
+        server: {
+          name: 'Amnennar',
+          slug: 'amnennar',
+        },
+        faction: 'Alliance',
+      },
+    });
+
     chrome.tabs.create({ url: './index.html' });
   }
 });
