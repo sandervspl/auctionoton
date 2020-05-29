af19b0058d804e385ed20d80fae3e3759e98ad0d
Sander Vispoel <sander.vispoel@labela.nl>
2020-05-29 18:01:44 +0200

    Zip dist folder with script

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,6 @@ import clearDist from 'rollup-plugin-delete';
 import { chromeExtension } from 'rollup-plugin-chrome-extension';
 import injectProcessEnv from 'rollup-plugin-inject-process-env';
 import svg from 'rollup-plugin-svg';
-import zip from 'rollup-plugin-zip';
 import config from './config';
 
 const rollup = [{
@@ -38,13 +37,5 @@ const rollup = [{
     typescript(),
   ],
 }];
-
-if (process.env.NODE_ENV === 'production') {
-  rollup[1].plugins.push(
-    zip({
-      dir: '.',
-    }),
-  );
-}
 
 export default rollup;
