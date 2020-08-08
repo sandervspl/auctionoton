f37d678fb3fb7d9987cad6c23338aacac240839b
sandervspl <sandervispoel@hotmail.com>
2020-08-08 18:21:44 +0200

    Clear browser cache of old data on browser startup

--- src/modules/background.ts
+++ src/modules/background.ts
@@ -1,10 +1,11 @@
-import AsyncStorage from 'utils/asyncStorage';
+import asyncStorage from 'utils/asyncStorage';
+import validateCache from 'utils/validateCache';
 
 
+// Open page for user's server/faction information after installation
 addon.runtime.onInstalled.addListener((details) => {
-  // Open page for user's server/faction information after installation
   if (details.reason === 'install') {
-    AsyncStorage.set({
+    asyncStorage.set({
       user: {
         region: 'us',
         server: {
@@ -17,4 +18,30 @@ addon.runtime.onInstalled.addListener((details) => {
 
     addon.tabs.create({ url: './form.html' });
   }
+});
+
+
+// Clean up browser storage cache on browser startup
+addon.runtime.onStartup.addListener(async () => {
+  const items = await asyncStorage.get('items');
+  const modItems = { ...items };
+
+  for (const server in items) {
+    for (const faction in items[server]) {
+      for (const itemName in items[server][faction]) {
+        const item = items[server][faction][itemName];
+
+        if (!validateCache(item)) {
+          delete modItems[server][faction][itemName];
+
+          if (__DEV__) {
+            // eslint-disable-next-line no-console
+            console.log(`Removed ${server}-${faction}-${itemName} from browser cache`);
+          }
+        }
+      }
+    }
+  }
+
+  await asyncStorage.set({ items: modItems });
 });
