6b7ca074777e43ac3a07252adfa6301b8ad598c5
Sander Vispoel <sandervispoel@hotmail.com>
2022-05-02 17:19:33 +0200

    Use railway url

--- packages/constants/index.ts
+++ packages/constants/index.ts
@@ -1,8 +1,8 @@
 export class API {
   static Url = {
     // development: 'https://5d9b-82-168-31-31.ngrok.io',
-    development: 'https://develop--auctionoton-server.herokuapp.com',
-    production: 'https://auctionoton-server.herokuapp.com',
+    development: 'https://auctionoton-production.up.railway.app',
+    production: 'https://auctionoton-production.up.railway.app',
   }[process.env.NODE_ENV];
 
   static ItemsRoot = 'items';
