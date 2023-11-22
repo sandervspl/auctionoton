cda28c1940337ec066ab84d4d90bed7c4864d628
Sander Vispoel <sandervispoel@hotmail.com>
2023-11-22 17:35:44 +0100

    Add lua2json script

--- apps/cloudflare/src/lua2json.ts
+++ apps/cloudflare/src/lua2json.ts
@@ -1,5 +1,8 @@
 import { parse } from 'lua-json';
 
-const file = await Bun.file('./src/db.lua').text();
+const file = await Bun.file('./src/input/AuctionDB.lua').text();
 
-await Bun.write('db.json', JSON.stringify(parse(file.replace('AuctionDBSaved =', 'return ')), null, 2));
+await Bun.write(
+  'db.json',
+  JSON.stringify(parse(file.replace('AuctionDBSaved =', 'return ')), null, 2),
+);
