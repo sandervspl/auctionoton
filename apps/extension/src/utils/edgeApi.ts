f9143cf84a9133a64bc32cbe9cca77488c04fa97
Sander Vispoel <sandervispoel@hotmail.com>
2024-02-29 23:27:35 +0100

    Use https

--- apps/extension/src/utils/edgeApi.ts
+++ apps/extension/src/utils/edgeApi.ts
@@ -8,7 +8,7 @@ export class EdgeAPI {
     development: 'http://localhost:3000/api',
     // development: 'https://auctionoton-api-valor.vercel.app/api',
     // production: 'https://auctionoton-api.vercel.app/api',
-    production: 'http://auctionoton-api.sandervspl.dev',
+    production: 'https://auctionoton-api.sandervspl.dev',
     // production: 'http://nooog88.168.119.167.208.sslip.io',
     // production: 'https://auctionoton-edge-api.vercel.app/api',
   }[process.env.NODE_ENV as string];
