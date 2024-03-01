50ac9df69caae7c4613f712f468a22e63f9795e5
Sander Vispoel <sandervispoel@hotmail.com>
2024-03-01 11:14:50 +0100

    Delete old dev/prod urls

--- apps/extension/src/utils/edgeApi.ts
+++ apps/extension/src/utils/edgeApi.ts
@@ -6,10 +6,6 @@ export const edgeAPI = rateLimit(axios.create(), { maxRequests: 3, perMillisecon
 export class EdgeAPI {
   static Url = {
     development: 'http://localhost:3000/api',
-    // development: 'https://auctionoton-api-valor.vercel.app/api',
-    // production: 'https://auctionoton-api.vercel.app/api',
     production: 'https://auctionoton-api.sandervspl.dev',
-    // production: 'http://nooog88.168.119.167.208.sslip.io',
-    // production: 'https://auctionoton-edge-api.vercel.app/api',
   }[process.env.NODE_ENV as string];
 }
