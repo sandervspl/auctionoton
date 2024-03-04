0937ed982d98fcbb6037780b438d81cc2ed0cbbe
Sander <sandervispoel@hotmail.com>
2024-03-04 11:03:06 +0100

    Allow different ports

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -79,6 +79,6 @@ const app = new Elysia()
       }),
     },
   )
-  .listen(3000);
+  .listen(process.env.PORT ?? 3000);
 
 console.log(`Auctionoton Server is running at ${app.server?.hostname}:${app.server?.port}`);
