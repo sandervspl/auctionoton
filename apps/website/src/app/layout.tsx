96bd2b9bf1710cd24a87092b39b4be82bb227b10
Sander Vispoel <sandervispoel@hotmail.com>
2024-03-06 19:00:12 +0100

    Update layout.tsx

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -16,25 +16,25 @@ type Props = i.NextLayoutProps;
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: {
-		default: 'Auctionoton',
-		template: '%s | Auctionoton',
-	},
-	metadataBase: new URL(SITE_URL || 'http://localhost:3000'),
-	manifest: '/manifest.json',
+  title: {
+    default: 'Auctionoton',
+    template: '%s | Auctionoton',
+  },
+  metadataBase: new URL(SITE_URL || 'http://localhost:3000'),
+  manifest: '/manifest.json',
 };
 
 const RootLayout: React.FC<Props> = ({ children }) => {
-	return (
-		<html lang="en" className="h-dvh bg-white antialiased">
-			<body className={cn('h-full min-h-full', inter.className)}>
-				<Providers>
-					{children}
-					{process.env.NODE_ENV !== 'production' && <SizeIndicator />}
-				</Providers>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" className="h-dvh bg-white antialiased">
+      <body className={cn('h-full min-h-full', inter.className)}>
+        <Providers>
+          {children}
+          {process.env.NODE_ENV !== 'production' && <SizeIndicator />}
+        </Providers>
+      </body>
+    </html>
+  );
 };
 
 export default RootLayout;
