9b0f9c5245d763ca142b0c5fec6e875983e40237
Sander Vispoel <sandervispoel@hotmail.com>
2024-03-07 23:42:06 +0100

    Fix build

--- apps/website/src/components/common/realm-dropdown.tsx
+++ apps/website/src/components/common/realm-dropdown.tsx
@@ -28,7 +28,7 @@ export function RealmDropdown() {
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
           <Button variant="outline" className="w-[150px] justify-start capitalize">
-            {params.item[0]?.replaceAll('-', ' ')} ({params.item[1].toUpperCase()})
+            {params.item[0]?.replaceAll('-', ' ')} ({params.item[1]?.toUpperCase()})
           </Button>
         </PopoverTrigger>
         <PopoverContent className="w-[200px] p-0" align="start">
@@ -42,7 +42,7 @@ export function RealmDropdown() {
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>
         <Button variant="outline" className="w-[150px] justify-start">
-          {params.item[0]}
+          {params.item?.[0]}
         </Button>
       </DrawerTrigger>
       <DrawerContent>
