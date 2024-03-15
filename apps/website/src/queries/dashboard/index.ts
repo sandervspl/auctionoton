38c61b0b7e784030da1c93fd4db5c580a98e776e
Sander Vispoel <sandervispoel@hotmail.com>
2024-03-15 14:47:57 +0100

    Query sections with user id

--- apps/website/src/queries/dashboard/index.ts
+++ apps/website/src/queries/dashboard/index.ts
@@ -1,6 +1,14 @@
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
 import { db } from 'db';
 
 export async function getDashboardSections() {
+  const { userId } = auth();
+  if (!userId) {
+    redirect('/?error=unauthorized');
+  }
+
   return db.query.dashboardSections.findMany({
     with: {
       items: {
@@ -13,6 +21,7 @@ export async function getDashboardSections() {
         },
       },
     },
+    where: (dashboardSections, { eq }) => eq(dashboardSections.userId, userId),
     orderBy: (dashboardSections, { asc }) => asc(dashboardSections.order),
   });
 }
