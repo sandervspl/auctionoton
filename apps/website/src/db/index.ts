ed4af220b1592ca757c331b660ec167f837d354a
Sander Vispoel <sandervispoel@hotmail.com>
2024-07-26 16:25:04 +0200

    Revert db init

--- apps/website/src/db/index.ts
+++ apps/website/src/db/index.ts
@@ -1,23 +1,24 @@
+import 'server-only';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
 import * as schema from './schema';
 
-// const queryClient = postgres(process.env.DB_URL!);
-// export const db = drizzle(queryClient, { schema });
+const queryClient = postgres(process.env.DB_URL!);
+export const db = drizzle(queryClient, { schema });
 
-let db: ReturnType<typeof drizzle>;
+// let db: ReturnType<typeof drizzle>;
 
-if (process.env.NODE_ENV === 'production') {
-  const queryClient = postgres(process.env.DB_URL!);
-  db = drizzle(queryClient, { schema });
-} else {
-  const queryClient = postgres(process.env.DB_URL!);
-  if (!global.db) {
-    global.db = drizzle(queryClient, { schema });
-  }
+// if (process.env.NODE_ENV === 'production') {
+//   const queryClient = postgres(process.env.DB_URL!);
+//   db = drizzle(queryClient, { schema });
+// } else {
+//   const queryClient = postgres(process.env.DB_URL!);
+//   if (!global.db) {
+//     global.db = drizzle(queryClient, { schema });
+//   }
 
-  db = global.db;
-}
+//   db = global.db;
+// }
 
-export { db };
+// export { db };
