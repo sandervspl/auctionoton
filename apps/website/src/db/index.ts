7d667ae02885339da7e4b2c075ce6649e0784451
Sander Vispoel <sandervispoel@hotmail.com>
2024-07-26 14:35:58 +0200

    Update db init

--- apps/website/src/db/index.ts
+++ apps/website/src/db/index.ts
@@ -3,5 +3,21 @@ import postgres from 'postgres';
 
 import * as schema from './schema';
 
-const queryClient = postgres(process.env.DB_URL!);
-export const db = drizzle(queryClient, { schema });
+// const queryClient = postgres(process.env.DB_URL!);
+// export const db = drizzle(queryClient, { schema });
+
+let db: ReturnType<typeof drizzle>;
+
+if (process.env.NODE_ENV === 'production') {
+  const queryClient = postgres(process.env.DB_URL!);
+  db = drizzle(queryClient, { schema });
+} else {
+  const queryClient = postgres(process.env.DB_URL!);
+  if (!global.db) {
+    global.db = drizzle(queryClient, { schema });
+  }
+
+  db = global.db;
+}
+
+export { db };
