f02a42b335bd3ed06970dc22611a46ed74178eb5
Sander Vispoel <sandervispoel@hotmail.com>
2024-07-30 21:54:04 +0200

    Add zsa

---
+++ apps/website/src/hooks/server-action-hooks.ts
@@ -0,0 +1,13 @@
+import { useInfiniteQuery, useMutation, useQuery } from '@tanstack/react-query';
+import { setupServerActionHooks } from 'zsa-react-query';
+
+const { useServerActionQuery, useServerActionMutation, useServerActionInfiniteQuery } =
+  setupServerActionHooks({
+    hooks: {
+      useQuery: useQuery,
+      useMutation: useMutation,
+      useInfiniteQuery: useInfiniteQuery,
+    },
+  });
+
+export { useServerActionInfiniteQuery, useServerActionMutation, useServerActionQuery };
