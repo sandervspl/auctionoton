10e69ca7f1910906475608b658a18d5f4b3d28d3
Sander Vispoel <sandervispoel@hotmail.com>
2024-07-31 19:01:32 +0200

    Show loader when changing faction

--- apps/website/src/components/common/faction-buttons.tsx
+++ apps/website/src/components/common/faction-buttons.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { useSettings } from 'hooks/use-settings';
+import * as React from 'react';
 import Link from 'next/link';
 
 import { setAuctionHouseIdCookie } from 'actions/cookie';
 import { useServerActionMutation } from 'hooks/server-action-hooks';
+import { useSettings } from 'hooks/use-settings';
 import { Button } from 'shadcn-ui/button';
+import { Loader2Icon } from 'lucide-react';
 
 type Props = {
   href?: {
@@ -25,29 +27,39 @@ export const FactionButtons = (props: Props) => {
 
 const FactionButton = (props: { faction: 'alliance' | 'horde'; href?: string }) => {
   const { settings, setFaction } = useSettings();
+  const [isPending, startTransition] = React.useTransition();
   const setAuctionHouseCookie = useServerActionMutation(setAuctionHouseIdCookie);
 
   if (!props.href) {
     return (
       <Button
         variant={settings.faction === props.faction ? 'default' : 'outline'}
+        className="flex items-center gap-2"
         onClick={() => {
-          setFaction(props.faction);
-          setAuctionHouseCookie.mutateAsync({
-            region: settings.region,
-            realmSlug: settings.realm,
-            faction: props.faction,
+          startTransition(async () => {
+            setFaction(props.faction);
+
+            await setAuctionHouseCookie.mutateAsync({
+              region: settings.region,
+              realmSlug: settings.realm,
+              faction: props.faction,
+            });
           });
         }}
       >
         {props.faction}
+        {isPending && <Loader2Icon className="animate-spin" size={16} />}
       </Button>
     );
   }
 
   return (
     <Button variant={settings.faction === props.faction ? 'default' : 'outline'} asChild>
-      <Link href={props.href} className="capitalize" onClick={() => setFaction(props.faction)}>
+      <Link
+        href={props.href}
+        className="capitalize flex items-center gap-2"
+        onClick={() => setFaction(props.faction)}
+      >
         {props.faction}
       </Link>
     </Button>
