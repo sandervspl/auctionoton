8ef11db746475cd1f869170a7211d97b9135a776
Sander Vispoel <sandervispoel@hotmail.com>
2024-08-06 17:28:57 +0200

    Fix missing items on spell page

--- apps/extension/src/hooks/useItemsFetcher.ts
+++ apps/extension/src/hooks/useItemsFetcher.ts
@@ -1,14 +1,12 @@
 import * as i from 'types';
-import { skipToken, useQueries } from '@tanstack/react-query';
+import { useQueries } from '@tanstack/react-query';
 
 import { fetchItemFromAPI } from '@/queries/item';
 
-import useUser from './useUser';
 import { useWowhead } from './useWowhead';
 import { getItemFromStorage } from '@/utils/storage';
 
 export function useItemsFetcher(itemIds: number[], auctionHouseId: number) {
-  const user = useUser();
   const { version } = useWowhead();
 
   return useQueries({
@@ -20,17 +18,14 @@ export function useItemsFetcher(itemIds: number[], auctionHouseId: number) {
         itemId,
       } as i.CachedItemDataClassic,
       queryFn: async () => {
-        if (!user.realm || !user.faction) {
-          return;
-        }
-
         // Check browser storage if item is stored
-        const cachedItem = getItemFromStorage(user.realm!.auctionHouseId, itemId);
+        const cachedItem = await getItemFromStorage(auctionHouseId, itemId);
+
         if (cachedItem) {
           return cachedItem;
         }
 
-        const fetchedItem = await fetchItemFromAPI(itemId, user.realm.auctionHouseId, version);
+        const fetchedItem = await fetchItemFromAPI(itemId, auctionHouseId, version);
 
         if (fetchedItem === 'NOT_FOUND' || fetchedItem === undefined) {
           return {
